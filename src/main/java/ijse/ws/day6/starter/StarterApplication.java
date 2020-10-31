3e572d7ec041a03c66e32fe4088a6903cba887cd
kavindadilshan <kavindadilshan97@gmail.com>
2020-10-31 15:59:23 +0530

    changes

---
+++ src/main/java/ijse/ws/day6/starter/StarterApplication.java
@@ -0,0 +1,15 @@
+package ijse.ws.day6.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(StarterApplication.class, args);
+		System.out.println("Hi");
+	}
+
+
+}
